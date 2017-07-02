b003ac0c50865d79ed750ef2a9cf09bfdcbde10b
Julia Hazer <juliahazer@yahoo.com>
2017-07-02 13:59:33 -0700

    working hamburger menu for nav

--- src/Navbar.js
+++ src/Navbar.js
@@ -78,15 +78,10 @@ class Navbar extends Component {
       <div>
       <NavbarBS className="NavbarBS" inverse fixedTop>
         <NavbarBS.Header>
-          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </button>
           <NavbarBS.Brand>
             <a className="navbarBrand" onClick="">Top Songs</a>
           </NavbarBS.Brand>
+          <NavbarBS.Toggle />
         </NavbarBS.Header>
 
         <NavbarBS.Collapse id="bs-example-navbar-collapse-1">
